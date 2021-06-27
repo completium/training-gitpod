5be54e0bf7297f7b187f13ec3d14e3f95759c006
Mathias Hiron <mathias.hiron@gmail.com>
2021-06-27 20:21:22 +0200

    fix contract

--- exemples/counter_two_numbers/counter_two_numbers_arl.js
+++ exemples/counter_two_numbers/counter_two_numbers_arl.js
@@ -4,9 +4,11 @@ const Completium = require('@completium/completium-cli');
 const test = async () => {
  const completium = new Completium ();
  await completium.originate('counter_two_numbers.arl');
- await completium.call('counter', { with: "(3,4)"});
- const storage = await completium.getStorage('counter');
- const counter = storage.toNumber();
- assert(counter == 3, "Invalid counter value");
+ await completium.call('counter_two_numbers', { with: "(3,4)"});
+ const storage = await completium.getStorage('counter_two_numbers');
+ const v1 = storage.v1.toNumber();
+ const v2 = storage.v2.toNumber();
+ assert(v1 == 9, "Invalid v2 value");
+ assert(v2 == 11, "Invalid v2 value");
 }
 test();
