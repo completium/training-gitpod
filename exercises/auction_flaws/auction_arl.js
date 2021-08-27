193f987dd9762368d52c1063879f6cb59da2f417
Benoît Rognier <benoit.rognier@edulera.com>
2021-08-27 17:50:48 +0200

    fix

--- exercises/auction_flaws/auction_arl.js
+++ exercises/auction_flaws/auction_arl.js
@@ -2,16 +2,16 @@ const assert = require('assert');
 const { deploy, getAddress } = require('@completium/completium-cli');
 
 const test = async () => {
-  const start = new Date(Date.now()).toString();
-  const stop  = new Date(Date.now() + 5 * 60 * 1000).toString();
+  const start = Date.now().toString();
+  const stop  = (Date.now() + 5 * 60 * 1000).toString();
   const [auction, _] = await deploy('auction.arl', {
     parameters : {  owner : getAddress("alice"), deadline : stop },
     test : true
   });
-  await auction.setNow(start);
+  await auction._setNow(start);
   await auction.bid({ as : "bob", amount : "10tz" });
   await auction.bid({ as : "carl", amount : "15tz" });
-  await auction.setNow(stop);
+  await auction._setNow(stop);
   await auction.collectTopBid({ as : "alice" });
   await auction.claim({ as : "bob" });
 }
