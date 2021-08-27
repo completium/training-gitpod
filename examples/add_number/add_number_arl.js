382cd87d69d81e272af931b6c507660b3f7a84fa
Benoît Rognier <benoit.rognier@edulera.com>
2021-08-27 18:47:29 +0200

    fix

--- examples/add_number/add_number_arl.js
+++ examples/add_number/add_number_arl.js
@@ -2,9 +2,9 @@ const assert = require('assert');
 const { deploy } = require('@completium/completium-cli');
 
 const test = async () => {
-    const [counter, _] = await deploy('add_number.arl');
-    await counter.default({ arg : { quantity : 2 }});
-    const storage = await counter.getStorage();
+    const [add_number, _] = await deploy('add_number.arl');
+    await add_number.default({ arg : { quantity : 2 }});
+    const storage = await add_number.getStorage();
     const count = storage.toNumber();
     assert(count == 4, "Invalid counter value");
 }
--- examples/counter/counter_arl.js
+++ examples/counter/counter_arl.js
@@ -3,7 +3,7 @@ const { deploy } = require('@completium/completium-cli');
 
 const test = async () => {
     const [counter, _] = await deploy('counter.arl');
-    await counter.default({ arg : { quantity : 1 }});
+    await counter.default({ });
     const storage = await counter.getStorage();
     const count = storage.toNumber();
     assert(count == 3, "Invalid counter value");
