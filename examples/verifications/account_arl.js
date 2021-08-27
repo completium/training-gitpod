cf860b8e87b647bad50cea85bec0b8dfeac9c398
Benoît Rognier <benoit.rognier@edulera.com>
2021-08-27 15:28:44 +0200

    fix

--- examples/verifications/account_arl.js
+++ examples/verifications/account_arl.js
@@ -1,8 +1,9 @@
 const assert     = require('assert');
-const { deploy } = require('@completium/completium-cli');
+const { deploy, getAddress } = require('@completium/completium-cli');
 
 const test = async () => {
-  const [account, _] = await deploy('account.arl');
+  const alice = getAddress("alice");
+  const [account, _] = await deploy('account.arl', { parameters : { owner : alice } });
   await account.add({ arg : { value : "3" }, as : "alice" });
   await account.sub({ as : "alice" });
   const storage = await account.getStorage();
